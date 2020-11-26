6f1f89dc80a158bdd39a2b0361bab81d9765b98c
TANIGUCHI Masaya <m@docs.casa>
2020-11-26 17:51:21 +0900

    Use responsive layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import React from "react";
-import GridLayout from "react-grid-layout";
+import { Responsive, WidthProvider} from "react-grid-layout";
 import ConstituentTree from "../components/ConstituentTree";
 import Derivation from "../components/Derivation";
 import Grammar from "../components/Grammar";
 import Sentence from "../components/Sentence";
+
+const ResponsiveGridLayout = WidthProvider(Responsive)
 
 const App: React.FC = () => {
   return (
@@ -13,20 +15,20 @@ const App: React.FC = () => {
         <title>CCG Playground</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GridLayout className="layout" cols={6} rowHeight={100} width={1800}>
-        <div key="sentence-form" data-grid={{ x: 0, y: 0, w: 3, h: 4 }}>
+      <ResponsiveGridLayout className="layout" rowHeight={100} cols={{lg: 12, md: 12, sm: 12, xs: 6, xss: 6}}>
+        <div key="sentence-form" data-grid={{ x: 0, y: 0, w: 6, h: 4 }}>
           <Sentence />
         </div>
-        <div key="constituent-tree-viewer" data-grid={{ x: 0, y: 4, w: 3, h: 4 }}>
+        <div key="constituent-tree-viewer" data-grid={{ x: 0, y: 4, w: 6, h: 4 }}>
           <ConstituentTree />
         </div>
-        <div key="grammar-viewer" data-grid={{ x: 3, y: 0, w: 3, h: 4 }}>
+        <div key="grammar-viewer" data-grid={{ x: 6, y: 0, w: 6, h: 4 }}>
           <Grammar />
         </div>
-        <div key="ccg-tree-viewer" data-grid={{ x: 3, y: 4, w: 3, h: 4 }}>
+        <div key="ccg-tree-viewer" data-grid={{ x: 6, y: 4, w: 6, h: 4 }}>
           <Derivation />
         </div>
-      </GridLayout>
+      </ResponsiveGridLayout>
     </>
   );
 };
